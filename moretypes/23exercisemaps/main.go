053979204918037f6f830e69590af3e7118cec31
Paul Soberanes <paulsoberanes@gmail.com>
2017-06-01 12:46:25 -0500

    Maps and function values

---
+++ moretypes/23exercisemaps/main.go
@@ -0,0 +1,11 @@
+package main
+
+// WordCount is the map function
+func WordCount(s string) map[string]int {
+
+	return map[string]int{"x": 1}
+}
+
+func main() {
+	WordCount("Go! I am learning")
+}
